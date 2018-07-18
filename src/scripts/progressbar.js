afe61c631d0b5aa2814fe494d322ff1d62dff99d
abhishedesign007 <abhishek@lollypop.design>
2018-07-18 13:32:57 +0530

    added create plan content

--- src/scripts/progressbar.js
+++ src/scripts/progressbar.js
@@ -1,13 +1,10 @@
 import $ from 'jquery';
 
 var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+// var left, opacity, scale; //fieldset properties which we will animate
+// var animating; //flag to prevent quick multi-click glitches
 
 $(".next").click(function () {
-  // if (animating) return false;
-  // animating = true;
-
   current_fs = $(this).closest('.step');
   next_fs = $(this).closest('.step').next();
 
@@ -18,36 +15,9 @@ $(".next").click(function () {
   //show the next fieldset
   next_fs.show();
   current_fs.hide();
-  //hide the current fieldset with style
-  // current_fs.animate({ opacity: 0 }, {
-  //   step: function (now, mx) {
-  //     //as the opacity of current_fs reduces to 0 - stored in "now"
-  //     //1. scale current_fs down to 80%
-  //     // scale = 1 - (1 - now) * 0.2;
-  //     // //2. bring next_fs from the right(50%)
-  //     // left = (now * 50) + "%";
-  //     // //3. increase opacity of next_fs to 1 as it moves in
-  //     opacity = 1 - now;
-  //     current_fs.css({
-  //       // 'transform': 'scale(' + scale + ')',
-  //       // 'position': 'absolute'
-  //     });
-  //     next_fs.css({ 'opacity': opacity });
-  //   },
-  //   // duration: 800,
-  //   complete: function () {
-  //     current_fs.hide();
-  //     animating = false;
-  //   },
-  //   //this comes from the custom easing plugin
-  //   // easing: 'easeInOutBack'
-  // });
 });
 
 $(".previous").click(function () {
-  // if (animating) return false;
-  // animating = true;
-
   current_fs = $(this).closest('.step');
   previous_fs = $(this).closest('.step').prev();
 
@@ -58,26 +28,24 @@ $(".previous").click(function () {
   //show the previous fieldset
   current_fs.hide();
   previous_fs.show();
-  //hide the current fieldset with style
-  // current_fs.animate({ opacity: 0 }, {
-  //   step: function (now, mx) {
-  //     //as the opacity of current_fs reduces to 0 - stored in "now"
-  //     //1. scale previous_fs from 80% to 100%
-  //     // scale = 0.8 + (1 - now) * 0.2;
-  //     // //2. take current_fs to the right(50%) - from 0%
-  //     // left = ((1 - now) * 50) + "%";
-  //     // //3. increase opacity of previous_fs to 1 as it moves in
-  //     opacity = 1 - now;
-  //     // current_fs.css({ 'left': left });
-  //     previous_fs.css({ 'opacity': opacity });
-  //   },
-  //   // duration: 800,
-  //   complete: function () {
-  //     current_fs.hide();
-  //     animating = false;
-  //   },
-  //   //this comes from the custom easing plugin
-  //   // easing: 'easeInOutBack'
-  // });
+});
+
+
+$(".inner-next").click(function () {
+  current_fs = $(this).closest('.inner-step');
+  next_fs = $(this).closest('.inner-step').next();
+
+  //show the next fieldset
+  next_fs.show();
+  current_fs.hide();
+});
+
+$(".inner-previous").click(function () {
+  current_fs = $(this).closest('.inner-step');
+  previous_fs = $(this).closest('.inner-step').prev();
+
+  //show the previous fieldset
+  current_fs.hide();
+  previous_fs.show();
 });
 
