443a4d5606d0f0bf44e4c70b7c1170be3d0b559b
chandru <chandru@lollypop.biz>
2018-07-18 19:26:00 +0530

    changes in popup component

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,21 +1,21 @@
 import $ from 'jquery'
 $(document).ready(function(){
-	var findClass = $("#main-content").find('.plan-selection-links li:first-child').hasClass('active');
-	console.log(findClass);
+	// var findClass = $("#main-content").find('.plan-selection-links li:first-child').hasClass('active');
+	// console.log(findClass);
 	// if(findClass) {
 	// 	$("#exampleModalLong").show();
 	// 	$("#modal-overlay").addClass('active');
 	// }
-	$("#exampleModalLong").hide();
-	$("#modal-overlay").removeClass('active');
-	$(".selection-link a").click(function(e){
+	// $("#exampleModalLong").hide();
+	// $("#modal-overlay").removeClass('active');
+	$(".selection-link").click(function(e){
 		e.preventDefault();
-		if(findClass) {
+		if($("#main-content").find('.plan-selection-links li').first()) {
 			$("#exampleModalLong").hide();
 			$("#modal-overlay").removeClass('active');
 		}
-		$("#exampleModalLong").hide();
-		$(this).parents("#main-content").find("#exampleModalLong").show();
+		$("#exampleModalLong").show();
+		// $(this).parents("#main-content").find("#exampleModalLong").show();
 		$("#modal-overlay").addClass('active');
 	})
 	$(".close-popup").click(function(e){
