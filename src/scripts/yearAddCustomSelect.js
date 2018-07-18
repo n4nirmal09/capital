e50883b8e32654cee82e9ef017e8c6e1d8558df2
radhamani <radhamani@lollypop.biz>
2018-07-18 21:49:58 +0530

    Year plan tab 4/3

---
+++ src/scripts/yearAddCustomSelect.js
@@ -0,0 +1,7 @@
+// Initialize custom select on year plan form
+
+const yearAddPlanEle = document.getElementById('yearAddPlan-form');
+if (yearAddPlanEle) {
+  console.log('custom select')
+  const yearAddPlaneForm = new CGForm(document.getElementById('yearAddPlan-form'));
+}
