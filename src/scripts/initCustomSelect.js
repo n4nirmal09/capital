ad190689730e54510b10b8408e0ad6c5853da058
Varun <varun@lollypop.biz>
2018-07-18 22:08:03 +0530

    learn 2nd stage revalute on progress

--- src/scripts/initCustomSelect.js
+++ src/scripts/initCustomSelect.js
@@ -5,3 +5,9 @@ if (savingPlanEle) {
   console.log('custom select')
   const savingPlaneForm = new CGForm(document.getElementById('savingPlan-form'));
 }
+
+const collegePlanEle = document.getElementById('collegePlan-form');
+if (collegePlanEle) {
+  console.log('custom select')
+  const collegePlanForm = new CGForm(document.getElementById('collegePlan-form'));
+}
